9ff2010533a9a563dcbe6bf990d975cb2ae05f42
Jean-Francis Bastien <jfbastien@petalmd.com>
2026-04-03 10:15:25 -0400

    favicon

---
+++ app/javascript/controllers/favicon_alert_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Swaps the favicon to the "notification on" bell when this controller connects.
+// Place data-controller="favicon-alert" on an element that is injected by a
+// Turbo Stream broadcast so that connect() fires on the stream update.
+export default class extends Controller {
+  connect() {
+    const favicon = document.getElementById("favicon")
+    if (favicon) {
+      favicon.href = "/favicon-bell-on.svg"
+    }
+  }
+
+  disconnect() {
+    const favicon = document.getElementById("favicon")
+    if (favicon) {
+      favicon.href = "/favicon-bell.svg"
+    }
+  }
+}
